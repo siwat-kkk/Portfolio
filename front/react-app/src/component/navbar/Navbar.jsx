667a91c6e78d5ae0058eda3d53f6fed2cd5149d1
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2024-07-02 00:45:47 +0700

    Highlight navbar tag

--- front/react-app/src/component/navbar/Navbar.jsx
+++ front/react-app/src/component/navbar/Navbar.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import About from "../../pages/About";
 import Nav from "./Nav";
 import { Link } from "react-router-dom";
 
-const plugin = require('tailwindcss/plugin');
+const plugin = require("tailwindcss/plugin");
+
+// ทำ CSS ให้ Component แล้ว import เข้ามา
 
 const Navbar = () => {
+  const [selectedItem, setSelectedItem] = useState("Home");
+
+  const handleClick = (item) => {
+    setSelectedItem(item);
+    console.log(item);
+  };
+
+  const getNavItemClass = (item) => {
+    return selectedItem === item ? "text-blue-500" : "text-white";
+  };
+
   return (
     <nav class="bg-white border-gray-200 dark:bg-gray-900">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -51,8 +64,10 @@ const Navbar = () => {
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <Link
+                className={`${getNavItemClass("Home")}` }
+                onClick={() => handleClick("Home")}
                 to="/home"
-                class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
               >
                 Home
@@ -60,30 +75,18 @@ const Navbar = () => {
             </li>
             <li>
               <Link
+                className={`${getNavItemClass("About")}`}
+                onClick={() => handleClick("About")}
                 to="/about"
                 class=" block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 About
               </Link>
             </li>
-            {/* <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li> */}
-            {/* <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li> */}
             <li>
               <Link
+                className={`${getNavItemClass("Contact")}`}
+                onClick={() => handleClick("Contact")}
                 to="/contact"
                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
