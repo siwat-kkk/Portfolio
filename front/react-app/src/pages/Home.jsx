5201c423b72a458e2f76d0fdaa6ddc1ac36bdaa9
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2024-07-03 05:50:49 +0700

    Add Card

--- front/react-app/src/pages/Home.jsx
+++ front/react-app/src/pages/Home.jsx
@@ -5,16 +5,20 @@ import "../css/Pages.css";
 function Home() {
   return (
     <div className="home-bg flex flex-col items-center justify-center">
+      <div class="card">
+        <h1 class="card-title">Card Title</h1>
+        <p class="card-content">This is some content inside the card.</p>
+        <a href="https://example.com" class="card-link" target="_blank">
+          Read More
+        </a>
+      </div>
       <h2 className="text-2xl font-bold">This is Home</h2>
       <div class="my-4 w-32 h-32 bg-purple-500 flex items-center justify-center text-white rounded-lg">
-  <p>Home Block</p>
-</div>
-      <p className="mt-4 text-lg">
-        Home is a simple
-      </p>
+        <p>Home Block</p>
+      </div>
+      <p className="mt-4 text-lg">Home is a simple</p>
     </div>
   );
 }
 
 export default Home;
-
