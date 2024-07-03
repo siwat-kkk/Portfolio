1fb66bcec8356d1a81369b3701b929f404238690
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2024-07-03 22:09:51 +0700

    try add card and positioning at home

--- front/react-app/src/pages/Home.jsx
+++ front/react-app/src/pages/Home.jsx
@@ -5,15 +5,17 @@ import "../css/Pages.css";
 function Home() {
   return (
     <div className="home-bg flex flex-col items-center justify-center">
-      <div class="card">
-        <h1 class="card-title">Card Title</h1>
-        <p class="card-content">This is some content inside the card.</p>
-        <a href="https://example.com" class="card-link" target="_blank">
-          Read More
-        </a>
+      <div className="card-container">
+        <div className="card">
+          <h1 className="card-title">Card Title</h1>
+          <p className="card-content">This is some content inside the card.</p>
+          <a href="https://example.com" className="card-link" target="_blank" rel="noopener noreferrer">
+            Read More
+          </a>
+        </div>
       </div>
       <h2 className="text-2xl font-bold">This is Home</h2>
-      <div class="my-4 w-32 h-32 bg-purple-500 flex items-center justify-center text-white rounded-lg">
+      <div className="my-4 w-32 h-32 bg-purple-500 flex items-center justify-center text-white rounded-lg">
         <p>Home Block</p>
       </div>
       <p className="mt-4 text-lg">Home is a simple</p>
