3be256b0e0a1e099836319857aa96ecac5cb0ebb
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2024-07-06 14:26:18 +0700

    Change Hompage style and Navbar

--- front/react-app/src/component/navbar/Navbar.jsx
+++ front/react-app/src/component/navbar/Navbar.jsx
@@ -2,11 +2,8 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import About from "../../pages/About";
 import Nav from "./Nav";
-import { Link } from "react-router-dom";
 
 const plugin = require("tailwindcss/plugin");
-
-// ทำ CSS ให้ Component แล้ว import เข้ามา
 
 const Navbar = () => {
   const [selectedItem, setSelectedItem] = useState("Home");
@@ -21,31 +18,26 @@ const Navbar = () => {
   };
 
   return (
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <NavLink
+          to="/home"
+          className="flex items-center space-x-3 rtl:space-x-reverse no-underline"
         >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8"
-            alt="Flowbite Logo"
-          />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Siwat Phrommak
           </span>
-        </a>
+        </NavLink>
         <button
           data-collapse-toggle="navbar-default"
           type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
           aria-controls="navbar-default"
           aria-expanded="false"
         >
-          <span class="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menu</span>
           <svg
-            class="w-5 h-5"
+            className="w-5 h-5"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -53,89 +45,59 @@ const Navbar = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
         </button>
-        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              {/* <Link
-                onClick={() => handleClick("Home")}
-                className={`${getNavItemClass("Home")} block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent` }
+              <NavLink
                 to="/home"
-                // class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
+                style={({ isActive, isPending, isTransitioning }) => {
+                  return {
+                    color: isActive ? "#b6fdf3" : "white",
+                    textDecoration: "none",
+                  };
+                }}
               >
                 Home
-              </Link> */}
-              <NavLink to={"Home"} 
-              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                aria-current="page"
-                style={({ isActive, isPending, isTransitioning }) => {
-                  return {
-                    // fontWeight: isActive ? "" : "",
-                    // color: isPending ? "red" : "white",
-                    color: isActive ? "#b6fdf3" : "white",
-                    // backgroundColor: isActive ? "Cyan" : "black",
-                    // viewTransitionName: isTransitioning ? "slide" : "",
-                    textDecoration: 'none'
-                  };
-                }}
-                > 
-                Home 
-                </NavLink>
-            </li>
-            <li>
-              {/* <Link
-                onClick={() => handleClick("About")}
-                className={`${getNavItemClass("About")} block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-                to="/about"
-                // class=" block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </Link> */}
-              <NavLink to={"About"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                aria-current="page"
-                style={({ isActive, isPending, isTransitioning }) => {
-                  return {
-                    // fontWeight: isActive ? "" : "",
-                    // color: isPending ? "red" : "white",
-                    color: isActive ? "#b6fdf3" : "white",
-                    // backgroundColor: isActive ? "Cyan" : "black",
-                    // viewTransitionName: isTransitioning ? "slide" : "",
-                    textDecoration: 'none'
-                  };
-                }}
-                > 
-                About 
               </NavLink>
             </li>
             <li>
-              {/* <Link
-                onClick={() => handleClick("Contact")}
-                className={`${getNavItemClass("Contact")} block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent`}
-                to="/contact"
-                // class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </Link> */}
-              <NavLink to={"Contact"} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              <NavLink
+                to="/about"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
                 style={({ isActive, isPending, isTransitioning }) => {
                   return {
-                    // fontWeight: isActive ? "" : "",
-                    // color: isPending ? "red" : "white",
                     color: isActive ? "#b6fdf3" : "white",
-                    // backgroundColor: isActive ? "Cyan" : "black",
-                    // viewTransitionName: isTransitioning ? "slide" : "",
-                    textDecoration: 'none'
+                    textDecoration: "none",
                   };
                 }}
-                > Contact </NavLink>
+              >
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                aria-current="page"
+                style={({ isActive, isPending, isTransitioning }) => {
+                  return {
+                    color: isActive ? "#b6fdf3" : "white",
+                    textDecoration: "none",
+                  };
+                }}
+              >
+                Contact
+              </NavLink>
             </li>
           </ul>
         </div>
