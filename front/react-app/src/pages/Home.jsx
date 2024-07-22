71ea8651435ceefa521a5030869c2f996a011e34
Siwat Phrommak <77423738+Siwat-2402@users.noreply.github.com>
2024-07-22 21:17:37 +0700

    and other content home

--- front/react-app/src/pages/Home.jsx
+++ front/react-app/src/pages/Home.jsx
@@ -4,7 +4,11 @@ import "../css/Pages.css";
 
 function Home() {
   return (
-    <div className="home-bg flex flex-col items-center justify-center">
+    <div className="home-bg flex">
+      <div className="non-card-container">
+        <h2 className="non-card-title">Non-card Content Title</h2>
+        <p className="non-card-content">This is some content inside the non-card content block.</p>
+      </div>
       <div className="card-container">
         <div className="card">
           <h1 className="card-title">Card Title</h1>
@@ -21,25 +25,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="card-container">
-        <div className="card">
-          <h1 className="card-title">Card Title</h1>
-          <p className="card-content">This is some content inside the card.</p>
-          <a
-            href="https://example.com"
-            className="card-link"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read More
-          </a>
-        </div>
-      </div>
-      {/* <h2 className="text-2xl font-bold">This is Home</h2>
-      <div className="my-4 w-32 h-32 bg-purple-500 flex items-center justify-center text-white rounded-lg">
-        <p>Home Block</p>
-      </div>
-      <p className="mt-4 text-lg">Home is a simple</p> */}
+      
     </div>
   );
 }
